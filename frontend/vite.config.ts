cbd3af43489d76b090ecb1315e26b49b4129aa32
AriFlow2016 <Johan.fbg@gmail.com>
2025-03-02 02:38:39 +0100

    Fix build error by simplifying dependencies and removing admin-lte

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,6 @@
-/// <reference types="vite/client" />
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: process.cwd(),
-  base: '/',
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    target: 'es2015',
-    minify: 'esbuild',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  plugins: [react()]
 });
