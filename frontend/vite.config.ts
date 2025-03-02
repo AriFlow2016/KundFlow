9e2fc7fc01a12d7a19d933496cefe3efff86c9ee
AriFlow2016 <Johan.fbg@gmail.com>
2025-03-02 02:23:59 +0100

    Fix Lifecycle script build error with improved configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: process.cwd(),
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
