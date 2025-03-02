9f0fe02c4b660b84c87fa1d85fb324820dc6ed2c
AriFlow2016 <Johan.fbg@gmail.com>
2025-03-02 02:06:07 +0100

    Simplify build configuration and remove tsc from build script

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      ...nodeResolve({
-        preferBuiltins: true,
-        browser: true
-      }),
-      enforce: 'pre',
-      apply: 'build'
-    },
-    {
-      ...commonjs({
-        include: /node_modules/
-      }),
-      enforce: 'pre',
-      apply: 'build'
-    }
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     target: 'es2015',
-    minify: 'esbuild',
-    sourcemap: true,
-    commonjsOptions: {
-      transformMixedEsModules: true
-    }
+    minify: 'esbuild'
   },
   resolve: {
     alias: {
